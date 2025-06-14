e0b47d238593324ef453887c3ac6d82965f891bb
Álvaro Villamarín <alvaro_90_xti@hotmail.com>
2025-06-14 15:29:02 +0200

    chore: remove unused muscle.helper

--- src/helpers/muscle.helper.ts
+++
@@ -1,8 +0,0 @@
-import muscleRepository from '../repositories/muscle.repository'
-
-async function areValidMuscles(muscles: string[]): Promise<boolean> {
-  const dbMuscles = await muscleRepository.findAll({ name: 1 })
-  return dbMuscles.every((muscle) => muscles.includes(muscle))
-}
-
-export { areValidMuscles }
