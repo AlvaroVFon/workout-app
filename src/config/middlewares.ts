845d19342258aa684ecc29025c70179ec2557df4
Álvaro Villamarín <alvaro_90_xti@hotmail.com>
2025-08-04 18:35:13 +0200

    chore: cors frontend premission

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -1,11 +1,17 @@
 import express, { Express } from 'express'
 import passport from './passport'
 import cors from 'cors'
+import { parameters } from './parameters'
 
 function configureMiddlewares(app: Express) {
   app.use(express.json())
   app.use(passport.initialize())
-  app.use(cors())
+  app.use(
+    cors({
+      origin: parameters.frontUrl,
+      credentials: true,
+    }),
+  )
 }
 
 export { configureMiddlewares }
